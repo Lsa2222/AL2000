d63d87ebfe5afff5794b5d9fb9e3a3d84a7f3821
LucasSauv <103965029+Lsa2222@users.noreply.github.com>
2022-11-21 14:41:52 +0100

    Update bdd.sql
    une version de la base de donné plus propre avec quelques modifications

--- src/BDD/bdd.sql
+++ src/BDD/bdd.sql
@@ -1,20 +1,17 @@
-drop table HistoriqueLocation
-drop table LesAbonnesEnfant
-drop table LesAbonnesAdultes
-drop table LesLocations
-drop table LesRestrictions
-drop table LesAbonnes
-drop table TagsFilm
-drop table LesFilms
-drop table LesBlueRay
-drop table LesLocationsQR
-drop table LesFilmsDisponibles
-drop table LesPersonnes
+drop table HistoriqueLocation;
+drop table LesLocationsBR;
+drop table LesRestrictions;
+drop table LesAbonnes;
+drop table TagsFilm;
+drop table LesBlueRay;
+drop table LesLocationsQR;
+drop table Tags;
+drop table LesFilms;
+drop table LesPersonnes;
 
 create table LesPersonnes (
     id integer not null,
-    isGuest boolean,
-    constraint LesPersonnes_1 primary key (id)
+    constraint LesPersonnes_Key primary key (id)
 );
 
 create table LesAbonnes(
@@ -22,53 +19,48 @@ create table LesAbonnes(
     prenom varchar2(20),
     nom varchar2(20),
     credit number(9),
-    constraint LesAbonnes_Key primary key (id, preno),
-    constraint LesAbonnes_FK foreign key (id) references LesPersonnes (id)
-    constraint LesAbonnes_money check (credit >= 0)
-);
-
-create table LesAbonnesAdultes(
-    id integer not null,
     adrMail varchar2(40),
     AdrPhy varchar2 (90),
-    constraint LesAbonnesAdultes_Key primary key (id, adrMail, AdrPhy),
-    constraint LesAbonnesAdultes_FK foreign key (id) references LesPersonnes (id)
-);
-
-create table LesAbonneEnfant(
-    id integer not null,
-    id_parent integer not null,
-    constraint LesAbonneEnfant_Key primary key (id, id_parent),
-    constraint LesAbonneEnfant_FKid foreign key (id) references LesPersonnes (id),
-    constraint LesAbonneEnfant_FKidparent foreign key (id_parent) references LesAbonnesAdultes (id)
+    constraint LesAbonnes_Key primary key (id),
+    constraint LesAbonnes_FK foreign key (id) references LesPersonnes (id),
+    constraint LesAbonnes_money check (credit >= 0)
 );
 
 create table LesFilms(
     noFilm integer not null,
     titre varchar2(20),
     realisateur varchar2(20),
-    genre varchar2(200)
+    genre varchar2(200),
     resumer varchar2(500),
     constraint LesFilms_key primary key (noFilm)
+);
+
+create table Tags(
+	tag varchar2(50),
+	constraint Tag primary key (tag)
 );
 
 create table TagsFilm(
     tag varchar2(20),
     noFilm integer not null,
     constraint TagsFilm_key primary key (tag, noFilm),
-    constraint TagsFilm_FK foreign key (noFilm) references LesFilms (noFilm)
+    constraint TagsFilm_Fktag foreign key (tag) references Tags (tag),
+    constraint TagsFilm_FKnoFilm foreign key (noFilm) references LesFilms (noFilm)
 );
 
-create table LesFilmsDisponibles(
-    noFilm integer not null,
-    constraint LesFilmsDisponibles_Key primary key (noFilm),
-    constraint LesFilmsDisponibles_FK foreign key (noFilm) references LesFilms (noFilm)
+create table LesRestrictions(
+	id integer not null,
+	tag varchar2(20),
+	constraint LesRestrictions_Key primary key (id, tag),
+	constraint LesRestrictions_Fkid foreign key (id) references LesPersonnes (id),
+	constraint LesRestrictions_Fktag foreign key (tag) references Tags (tag)
 );
 
 create table LesLocationsQR(
     id integer not null,
     noFilm integer not null,
     dateActivation date,
+    dateAchat date,
     constraint LesLocationsQR_Key primary key (id, noFilm, dateActivation),
     constraint LesLocationsQR_FKid foreign key (id) references LesPersonnes (id),
     constraint LesLocationsQR_FKnoFIlm foreign key (noFilm) references LesFilms (noFilm)
@@ -77,15 +69,15 @@ create table LesLocationsQR(
 create table LesBlueRay(
     idBR integer not null,
     noFilm integer not null,
-    etat varchat2(4),
+    etat varchar2(4),
     constraint LesBlueRay_Key primary key (idBR),
     constraint LesBlueRay_FKnoFilm foreign key (noFilm) references LesFilms (noFilm)
 );
 
-create table LesLocations(
+create table LesLocationsBR(
     id integer not null,
     noFilm integer not null,
-    idBR integer not null
+    idBR integer not null,
     dateDebut date,
     constraint LesLocations_Key primary key (id, noFilm, idBR),
     constraint LesLocations_Fkid foreign key (id) references LesPersonnes (id),
@@ -95,12 +87,10 @@ create table LesLocations(
 create table HistoriqueLocation (
     id integer not null,
     noFilm integer not null,
-    idBR integer not null,
+    idBR integer, /* Si égal à 0, alors QR*/
     dateDebut date,
     dateFin date,
-    constraint HistoriqueLocation_Key primary key (id, noFilm, idBR, dateDebut, dateFin),
+    constraint HistoriqueLocation_Key primary key (id, noFilm, idBR),
     constraint HistoriqueLocation_FKid foreign key (id) references LesPersonnes (id),
-    constraint HistoriqueLocation_FKnoFilm foreign key (noFilm) references LesFilms (noFilm),
-    constraint HistoriqueLocation_FKidBR foreign key (idBR) references LesLocations (idBR),
-    constraint HistoriqueLocation_FkdateDebut foreign key (dateDebut) references LesLocations (dateDebut)
+    constraint HistoriqueLocation_FKnoFilm foreign key (noFilm) references LesFilms (noFilm)
 );
