554e8d201ddf4eb0ddb50853def6bc6fdd5bed74
besantej <83356167+besantej@users.noreply.github.com>
2022-12-12 09:28:13 +0100

    test

--- src/externe/Main.java
+++ src/externe/Main.java
@@ -3,7 +3,9 @@ package externe;
 import ui.*;
 import fc.*;
 
+import java.io.PrintStream;
 import java.math.BigInteger;
+import java.util.Iterator;
 
 import BDD.*;
 
@@ -15,17 +17,60 @@ public class Main {
 		   t.update_Catalogue_1(c);
 		   
 		   FacadeBD bd =  FacadeBD.creer();
+		   bd.reset();
 		   FacadeTLI tli = new FacadeTLI();	   
-		   Test test = new Test(bd,tli,c);
-		   test.test1();
-		   test.test2();
+//		   Test test = new Test(bd,tli,c);
+//		   test.test1();
+//		   test.test2();
 		   
-		   
-		   
-		   
-//		   f.fenetre_origine(false);
+//		   testLocBR(System.out);
+		   fenetre f = new fenetre();
+		   f.fenetre_origine(false);
 		   
 
-
-	   }
+	   }	  
+//	   
+//	   static void testLocBR(PrintStream out) {
+//		   FacadeBD fbd = FacadeBD.creer();
+//	        out.println("Add loc br = ");
+//	        Abonne a = fbd.getAbonne(1);
+//	        
+//	        //BluRay br, Personne p
+//	        
+//	        BluRay b = new BluRay(6,null,true);
+//	        LocationBR l = new LocationBR(b,a);
+//	        
+//	        out.println(a.toString());
+//	        Iterator<LocationBR> locBr = a.getLocBr().iterator();
+//	        while(locBr.hasNext()) {
+//	            out.println("    "  + locBr.next().toString());
+//	        }
+//	        
+//	        if(fbd.newLocation(l)) {
+//	            out.println("Location reussie !");
+//	        } else {
+//	            out.println("echec");
+//	            return;
+//	        }
+//	        a = fbd.getAbonne(1);
+//	        
+//	        out.println(a.toString());
+//	        locBr = a.getLocBr().iterator();
+//	        while(locBr.hasNext()) {
+//	            out.println("    "  + locBr.next().toString());
+//	        }
+//	        
+//	        if(fbd.delLocation(l)) {
+//	            out.println("Suppression reussie !");
+//	        } else {
+//	            out.println("echec");
+//	            return;
+//	        }
+//	        a = fbd.getAbonne(1);
+//	        
+//	        out.println(a.toString());
+//	        locBr = a.getLocBr().iterator();
+//	        while(locBr.hasNext()) {
+//	            out.println("    "  + locBr.next().toString());
+//	        }
 }
