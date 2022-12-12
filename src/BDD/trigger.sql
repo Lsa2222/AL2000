ee30622af191fb0ecd8cd829cf55ccea7d2cf048
besantej <83356167+besantej@users.noreply.github.com>
2022-12-12 16:51:03 +0100

    Update trigger.sql

--- src/BDD/trigger.sql
+++ src/BDD/trigger.sql
@@ -13,3 +13,15 @@ begin
     insert into HistoriqueLocation values (:old.id, :old.noFilm, 0, :old.dateAchat, SYSDATE);
 end;
 /
+
+create or replace trigger guestLocation
+after insert on LesLocationsBR
+declare
+    nb_max integer;
+begin
+    select max(count(*)) into nb_max from LesPersonnes p, LesLocationsBR l where p.id in (select p.id from LesPersonnes p minus(select a.id from lesabonnes a)) and l.id=p.id group by p.id;
+    if (nb_max > 1) then 
+        RAISE_APPLICATION_ERROR(-20154, 'il y a deja 1 films en location actuellement');
+    end if;
+end;
+/
